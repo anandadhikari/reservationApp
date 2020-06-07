fa3a5168d8d376f023da3ef1f1b0af38bdf6f04f
Anand <anandadhikari02@gmail.com>
2020-06-07 15:04:58 +0530

    first commit

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
-
+const cors = require('cors')
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use(express.json());
 
